e76bd5c74643ffed0d446fc63cf657bf59028cbe
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-09-22 23:35:58 +0100

    Set up migration

---
+++ scripts/migration-create.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+const yargs = require("yargs")
+
+const {execSync} = require("child_process")
+
+const {
+   _: [name],
+   path,
+
+} = yargs.argv;
+
+const migrationPath = `src/database/migrations/${name}`;
+
+execSync(`typeorm migration:create ${migrationPath}`, {studio: "inherit" });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import * as dotenv from "dotenv";
 
 import app from './app';
+import { AppDataSource } from "./database/data-source";
 
 dotenv.config();
+
 const PORT = process.env.PORT || 3000
+
+AppDataSource.initialize().then(async ()=> {
+   console.log("Database connected successfully!")
+})
+.catch((err) => console.log(err))
+
 app.listen(PORT, () => {
    console.log(`App running on port ${PORT}`);
 });
